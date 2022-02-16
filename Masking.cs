c3a3973e17746746cb28bcfab90cdd3785e33a2b
Silas Maughan <silas.maughan@gmail.com>
2022-02-16 21:37:41 +0000

    Masking

---
+++ Masking.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maze_Generation_3
+{
+    class Masking : MazeAlgorithms
+    {
+        public int count = 0;
+        Stack<Coord> history = new Stack<Coord>();
+        Random rnd = new Random();
+        public void Generate(cell[,] Board, MazeSetup setup, int mazeHeight, int mazeLength)
+        {
+            this.Board = Board;
+            int xPos = setup.xPos;
+            int yPos = setup.yPos;
+            for(int i = 0; i< mazeHeight; i++)
+            {
+                Board[i, i].valid = false;
+            }
+            
+        }
+        public struct Coord
+        {
+            public int x;
+            public int y;
+
+            public Coord(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
+    }
+}
